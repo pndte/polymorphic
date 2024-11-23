e48acdf68aae8184601db405ebcff4f7d5ec0a40
pndte <baggerbig1@gmail.com>
2024-11-23 15:48:04 +0300

    feat(entities): added health with reactive fields

---
+++ Assets/Scripts/Motion/ChaseCamera.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Motion
+{
+    public class ChaseCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _speed;
+        private Transform _transform;
+
+        private void Awake()
+        {
+            Init(); // TODO: Load pool
+        }
+
+        public void Init()
+        {
+            _transform = transform;
+        }
+
+        private void FixedUpdate()
+        {
+            var smoothedPosition = Vector2.Lerp(_transform.position, _target.position, _speed);
+            _transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, _transform.position.z);
+        }
+    }
+}
