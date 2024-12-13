ec9012988e57bbf28b18119e4e2b6d17e5700aea
pndte <baggerbig1@gmail.com>
2024-12-13 22:20:34 +0300

    feat(entities): removed the define direction function from PhysicsMovement.cs

--- Assets/Scripts/Entities/Gameplay/Motion/PhysicsMovement.cs
+++ Assets/Scripts/Entities/Gameplay/Motion/PhysicsMovement.cs
@@ -18,16 +18,5 @@ namespace PEntities.Gameplay.Motion
         {
             _physics.velocity = direction * _config.Speed;
         }
-
-        private void DefineDirection()
-        {
-            // if (Input.GetKey(KeyCode.A)) _cachedDirection.x = Mathf.Clamp(_cachedDirection.x - _config.DirectionIncreaseSpeed, -1, 0);
-            // else if (Input.GetKey(KeyCode.D)) _cachedDirection.x = Mathf.Clamp(_cachedDirection.x + _config.DirectionIncreaseSpeed, 0, 1);
-            // else _cachedDirection.x = 0;
-            //
-            // if (Input.GetKey(KeyCode.S)) _cachedDirection.y = Mathf.Clamp(_cachedDirection.y - _config.DirectionIncreaseSpeed, -1, 0);
-            // else if (Input.GetKey(KeyCode.W)) _cachedDirection.y = Mathf.Clamp(_cachedDirection.y + _config.DirectionIncreaseSpeed, 0, 1);
-            // else _cachedDirection.y = 0;
-        }
     }
 }
